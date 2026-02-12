fd667d91f3c0fd69a5faec204703d0923d20d1ee
CirusThaVirus <161253210+CirusTV@users.noreply.github.com>
2026-02-11 20:22:18 -0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        neonCyan: '#00f0ff',
+        neonPink: '#ff00ff',
+        neonPurple: '#9d00ff',
+      },
+    },
+  },
+  plugins: [],
+}
